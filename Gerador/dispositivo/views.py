a8fc72ccf2ab6c27e02e2d9150f2313042be23de
denielfer <dfc152@gmail.com>
2021-10-23 18:40:24 -0300

    mudando gerador para usar nome como id e adicionando a biblioteca nos requirements

--- Gerador/dispositivo/views.py
+++ Gerador/dispositivo/views.py
@@ -2,14 +2,10 @@ from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.template import loader
 from .dispositivo import Dispositivo
-from random import choices
-from string import hexdigits
+import names
 from .import mqtt_handler
 
 dispositivos = {}
-
-def get_random_mac_id() -> str:
-    return ':'.join(''.join(choices(hexdigits, k = 2)).upper() for _ in range(6))
 
 def home(request):
     template = loader.get_template("home.html")
@@ -18,9 +14,9 @@ def home(request):
 def add(request):
     if(request.method == "POST"):
         for _ in range(int(request.POST.get('numero_de_dispositivos'))):
-            id = get_random_mac_id()
+            id = names.get_full_name()
             while(id in dispositivos):
-                id = get_random_mac_id()
+                id = names.get_full_name()
             # while(id not in mqtt_handler.dispositivo_fog):
             #     print("esperando fog")
             #     mqtt_handler.get_fog(id)
